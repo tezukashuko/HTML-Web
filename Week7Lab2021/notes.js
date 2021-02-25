7043a8daee13cfcab9e93e89941dad13493e5550
tezukashuko <tezukashuko@gmail.com>
2021-02-25 23:19:52 +0700

    Revert "update cty web"
    This reverts commit 7fa6234d45363010abdbf15b6991e071b7f4f554.

--- Week7Lab2021/notes.js
+++
@@ -1,5 +0,0 @@
-let b = activeEdits.filter(function(edit){
-    if(edit[0].textContent == 2) {
-    return true;
-    }
-})
--- Week8Lect2021/main.js
+++
@@ -1,2 +0,0 @@
-let loadbox= document.querySelector(".loadbox");
-loadbox.style = 'display= none'
